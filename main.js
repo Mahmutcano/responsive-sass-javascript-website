c6c319e3d698c515795d98496644ceb9c15db42d
Mahmut Can Özgan <ozgancan9@gmail.com>
2022-03-13 14:23:17 +0300

    Responsive

---
+++ main.js
@@ -0,0 +1,41 @@
+const navItems = document.querySelector("#nav_items");
+const openNavBtn = document.querySelector("#open_nav_btn");
+const closeNavBtn = document.querySelector("#close_nav_btn");
+
+openNavBtn.addEventListener("click", () => {
+    navItems.style.display = "flex";
+    openNavBtn.style.display = "none";
+    closeNavBtn.style.display = "inline-block";
+})
+
+const closeNav = () => {
+    navItems.style.display = "none";
+    openNavBtn.style.display = "inline-block";
+    closeNavBtn.style.display = "none";
+}
+
+closeNavBtn.addEventListener("click", closeNav);
+
+// SWİPER SLİDE
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+        // RESPONSİVE BREAKPOİNTS
+        breakpoi̇nts: {
+            // Window width is >= 600px
+            600: {
+                slidesPerView: 1,
+    
+            },
+    
+            // Window width is >= 1024px
+            1024: {
+                slidesPerView: 2,
+    
+            }
+        }
+  });
